3705cca2b57e1d3d579cc1477d9910a4d31ed0bd
bryoh <brianbryo@gmail.com>
2018-01-06 21:39:57 +0000

    SQUISH: Empty functions as TODO placeholders

--- squish_objects/q_t_objects.py
+++ squish_objects/q_t_objects.py
@@ -63,6 +63,7 @@ def _findChildrenByProps_func(parent_obj, properties_names_values, max_child_cou
 
     return found_children
 
+
 def _hasPropertiesAndValues(obj, properties_names_values):
     # Verify type early to minimize property value comparisons
     if "type" in properties_names_values and \
@@ -74,4 +75,88 @@ def _hasPropertiesAndValues(obj, properties_names_values):
     """
     return True
 
+
+
+def filterObjectListByProps(objectList, props):
+    return [ obj for obj in objectList if _hasPropertiesAndValues(obj, props) ]
+
+
+def getNextObject(properties, parent=None):
+    """ cycle through each instance of an object
+    Perfect for situations like when you need to click objects that have the same properties 
+    example radio buttons
+    """
+
+
+    objectsOfInterest = []
+    if _hasPropertiesAndValues(obj=parent, properties_names_values=properties):
+        objectsOfInterest = [parent, ]
+
+    objectsOfInterest += findDescendantsByProps(parent_obj=parent, properties_names_values=properties, max_child_count=-1, depth=-1)
+
+    for occurence in range(len(objectsOfInterest)):
+        yield getObject(properties=properties, parent=parent, occurence=occurence)
+
+
+def getObject( properties, parent=None, occurence=0, visible=False ):
+    """ properties can be dictionary(json), a symbolicName in the objectMap or a QT Object"""
+    if isinstance(properties, dict):
+        if _hasPropertiesAndValues(obj=parent,properties_names_values=properties):
+            result = parent
+        else: 
+            try: 
+                result = findDescendantsByProps(parent_obj=parent, properties_names_values=properties, max_child_count=occurence + 1, depth=-1)[occurence]
+
+            except IndexError: 
+                pass 
+    elif isinstance( properties, basestring ):
+        #QT/Squish symbolicNames  are sting 
+        if properties.startswith(':'):
+            assertNameInObjectMap(properties)
+        result = obj_getter(properties)
+    else: 
+        # The properties argument is already an object 
+        # Example in situations when a getter function is used in different scenarios
+        # like waitFor or  regetting the same object multiple times 
+        result = properties
+    return result
+
+
+def assertNameInObjectMap(name):
+    """ check if a symbolicName exists in the objectMap.
+    If it does not use the difflib library and log the closest match
+    """
+    objMap = objectMap.symbolicNames()
+    if name not in objMap:
+        potential = difflib.get_close_matches(name, objMap)
+        msg = "The symbolic name {!r} is not in the objectMap, did you mean one of: {!r}".format(name, potential)
+        #test.fatal(msg)
+        raise RuntimeError(msg)
+
+   
+def obj_getter(properties):
+    """ Call findDescendantsByProps/findChildrenByProps? """
+    pass
+
+
+
+
+def isPresent(myObjectOrName): pass
+
+def isVisible(myObjectOrName): pass
+
+
+def isEnabled(myObjectOrName): pass
+
+
+def getText(obj, property='text'):
+    """ find the text of an object
+    property = text/shortNameText or whatever the developer decided to name the QML/QT text field
+    """
+    pass
+
+
+def getImagePath(obj):
+    """ return the source path of a QT/QML object"""
+    pass 
 
